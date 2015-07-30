70ecfbdf7b41e3d8327c4889351db04427a1dbc0
elana <aparnaelangovan@gmail.com>
2015-07-30 11:01:16 +1000

    Add comments on DFS edge types

--- AE.Graphs.Core/DepthFirstSearchEdgeType.cs
+++ AE.Graphs.Core/DepthFirstSearchEdgeType.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// Represents the type of edge for depth first search traversal.
     /// </summary>
+    /// <seealso cref="http://courses.csail.mit.edu/6.006/spring11/rec/rec13.pdf" />
     public enum DepthFirstSearchEdgeType
     {
         ForwardEdge,
