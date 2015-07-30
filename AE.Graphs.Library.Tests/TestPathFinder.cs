fdaab61e294f97cdafd64ee45b903e1ca9f84de2
elangovana <aparnaelangovan@hotmail.com>
2015-07-30 20:12:26 +1000

    Fix  Test Path Finder

--- AE.Graphs.Library.Tests/TestPathFinder.cs
+++ AE.Graphs.Library.Tests/TestPathFinder.cs
@@ -7,30 +7,23 @@ namespace AE.Graphs.Library.Tests
     ///     Summary description for TestCycleFinder
     /// </summary>
     [TestFixture]
-    public class TestPathinder
+    public class TestPathFinder
     {
-        [Test]
-        public void TestPathCountCase1()
+        [TestCase(" AB5 - BC4 - CD8 - DC8 - DE6 - AD5 - CE2 - EB3 - AE7", 'A', 'C',4)]
+        public void ShouldCountSimplePathsGivenSourceAndDestinationNode(string igraph, char isourceNode, char idestinationNode, int expectedNofPaths)
         {
-            const string graphString = "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7";
-            var graph = GetGraph(graphString);
-            var actual = new PathFinder<Char>().FindAllSimplePaths(graph, 'A', 'C');
+           //Arrange
+            var graph = GraphLoaderHelper.LoadGraphFromString(igraph);
+            var sut = new PathFinder<Char>();
 
-            Assert.AreEqual(4, actual.Count);
+            //Assert
+            var actual = sut.FindAllSimplePaths(graph, isourceNode, idestinationNode);
+
+            //Act
+            Assert.AreEqual(expectedNofPaths, actual.Count);
         }
 
 
-        private RailwayNetworkWeightedDigraph<char> GetGraph(string graphString)
-        {
-            var graph = new RailwayNetworkWeightedDigraph<char>();
-            const string Gseparator = ", ";
-
-            var edgeStrings = graphString.Split(new[] {Gseparator}, StringSplitOptions.None);
-            foreach (var edgeString in edgeStrings)
-            {
-                graph.AddEdge(edgeString[0], edgeString[1], Convert.ToInt32(edgeString[2].ToString()));
-            }
-            return graph;
-        }
+       
     }
 }
