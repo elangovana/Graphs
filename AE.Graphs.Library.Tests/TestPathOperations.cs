47a21eb272bd17c19dfa25600e7e30cf7fbfa13c
elangovana <aparnaelangovan@hotmail.com>
2015-07-30 20:06:26 +1000

    Fix Test Path Operations

--- AE.Graphs.Library.Tests/TestPathOperations.cs
+++ AE.Graphs.Library.Tests/TestPathOperations.cs
@@ -6,27 +6,20 @@ namespace AE.Graphs.Library.Tests
     [TestFixture]
     public class TestPathOperations
     {
-        [Test]
-        public void TestPathCountCase2()
+        [TestCase("AB5 - BC4 - CD8 - DC8 - DE6 - AD5 - CE2 - EB3 - AE7", 'A', 'C', 4, 3)]
+        public void ShouldCountPathsWhenGivenSourceDestinationAndNumberOfStops(string igraph, char isourceNode, char idestinationNode, int inoOfStops, int expectedNoOfPaths)
         {
-            const string graphString = "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7";
-            var graph = GetGraph(graphString);
-            var actual = new PathOperations<Char>().FindAllPaths(graph, 'A', 'C', 4);
+           //Arrange
+            var graph = GraphLoaderHelper.LoadGraphFromString(igraph);
+            var sut = new PathOperations<Char>();
 
-            Assert.AreEqual(3, actual.Count);
+            //Assert
+            var actual =sut.FindAllPaths(graph, isourceNode, idestinationNode, inoOfStops);
+
+            //Atc
+            Assert.AreEqual(expectedNoOfPaths, actual.Count);
         }
 
-        private RailwayNetworkWeightedDigraph<char> GetGraph(string graphString)
-        {
-            var graph = new RailwayNetworkWeightedDigraph<char>();
-            const string Gseparator = ", ";
-
-            var edgeStrings = graphString.Split(new[] {Gseparator}, StringSplitOptions.None);
-            foreach (var edgeString in edgeStrings)
-            {
-                graph.AddEdge(edgeString[0], edgeString[1], Convert.ToInt32(edgeString[2].ToString()));
-            }
-            return graph;
-        }
+      
     }
 }
